8ee75acdbe7a4bc01f47d087eeff98e043ff7964
Mohit Malik <mohit1malik@gmail.com>
2022-08-03 00:33:37 +0530

    Interview question 1

--- src/test/java/Interviews/ArogyeSetu.java
+++ src/test/java/Interviews/ArogyeSetu.java
@@ -1,47 +1,80 @@
 package Interviews;
 
 
-import io.restassured.module.jsv.JsonSchemaValidator;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.json.simple.JSONObject;
-import org.testng.annotations.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
 
-import java.lang.reflect.Array;
-
-import static io.restassured.RestAssured.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ArogyeSetu {
 
     String stateid,stateidnonTrimed = null;
     String var = null;
     @Test
-    public void getStatesList(){
+    public void getStatesList() throws IOException {
 
-        Response response = given().
-                when().get("https://cdn-api.co-vin.in/api/v2/admin/location/states");
+        String maxCityName = null;
+        int maxCityNameLength = 0;
+        JSONObject stateObjectResponse =  readJsonFromUrl("https://cdn-api.co-vin.in/api/v2/admin/location/states");
 
-        JsonPath extractor = response.jsonPath();
+        JSONArray array = stateObjectResponse.getJSONArray("states");
 
-        stateidnonTrimed = extractor.getString("states.state_id");
-        stateid = stateidnonTrimed.trim();
-//        System.out.println(stateid);
-//        System.out.println(stateid.length());
-
-        for(int i =0; i <= stateid.length(); i++){
-//            System.out.println(i);
-
-            Response response1 = given().when().get("https://cdn-api.co-vin.in/api/v2/admin/location/districts/"+i);
-
-            JsonPath extractor1 = response1.jsonPath();
-            System.out.println(response1.body().asString());
-
-            String district_name = extractor.getString("districts.district_id");
-            System.out.println(district_name);
-
-
+        Set<Integer> stateIds = new HashSet<>();
+        for(int i = 0; i < array.length();i++){
+            JSONObject obj = array.getJSONObject(i);
+            stateIds.add(obj.getInt("state_id"));
         }
+        System.out.println(stateIds.size());
+
+        for(int stateId :stateIds){
+            JSONObject districtObjectResponse = readJsonFromUrl("https://cdn-api.co-vin.in/api/v2/admin/location/districts/"+ stateId);
+            JSONArray districtArray = districtObjectResponse.getJSONArray("districts");
+            for(int i = 0; i < districtArray.length();i++){
+                JSONObject obj = districtArray.getJSONObject(i);
+                String districtName = obj.getString("district_name");
+                if(districtName.length() > maxCityNameLength){
+                    maxCityNameLength = districtName.length();
+                    maxCityName = districtName;
+
+                }
+            }
+        }
+        System.out.println(maxCityName);
+
     }
 
+
+    @Test
+    public void Testget1() {
+
+
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
 
 }
